ec1a143518f931cbce5043582764dc5c61d18f5d
Peter Richmond <prichmond.dev@gmail.com>
2016-03-14 12:52:17 +1100

    9_backend_init: changed port retrieval

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    port = process.env.PORT || 3000;
 
 
 app.get('/healthCheck', (req, res) => {
     res.status(200).send('OK');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Example app listening on port 3000!');
 });
 
